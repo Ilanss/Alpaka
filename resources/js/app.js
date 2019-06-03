003fbd1d81228155d6d141d0f33e6ae26691555a
Teo Colomberotto <teo.colomberotto@heig-vd.ch>
2019-06-03 22:54:19 +0200

    "Vue.js_architecture_fix_BootstrapVueadded

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,10 @@
 
 
 require('./bootstrap');
+import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
 window.Vue = require('vue');
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
