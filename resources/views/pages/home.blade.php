306bc4813a0ce65a5e11a5a2cc0feadf488e2ef4
Skyggen <adrien.ciampone@heig-vd.ch>
2019-06-04 17:20:18 +0200

    merge

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,11 +1,8 @@
 @extends('layouts.app')
 @section('content')
-<<<<<<< HEAD
-=======
 <navbar-section></navbar-section>
 <slider-section></slider-section>
 <product-card></product-card>
 <brand-section></brand-section>
->>>>>>> frontend
 
 @endsection
