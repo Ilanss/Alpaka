030a8aa8d811d81abfe62674296e7d1fbd6e016c
Teo Colomberotto <teo.colomberotto@heig-vd.ch>
2019-06-04 11:25:15 +0200

    "Add_productCard

---
+++ resources/js/components/product_card/Product_card.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>
+      <b-card
+        title="Card Title"
+        img-src="https://picsum.photos/600/300/?image=25"
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="max-width: 20rem;"
+        class="mb-2"
+      >
+        <b-card-text>Lorem ipsum dolor sit amet consectetur adipisicing elit. In expedita nobis perspiciatis ex libero et?</b-card-text>
+
+        <b-button href="#" variant="primary">Ajouter au panier</b-button>
+      </b-card>
+    </div>
+  </div>
+</template>
+<script src="./Product_card.js"></script>
+<style src="./Product_card.css" scoped></style>
