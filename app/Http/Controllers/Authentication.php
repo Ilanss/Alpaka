d074a80696eb56ec3ebd186793218df5886605a8
Malo Senpai <malorie.genoud@gmail.com>
2019-06-05 10:36:22 +0200

    display pages according to the user's status

--- app/Http/Controllers/Authentication.php
+++ app/Http/Controllers/Authentication.php
@@ -8,9 +8,14 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
-class Login extends Controller
+class Auhtentification extends Controller
 {
-    public function auth(Request $request)
+    // Show form registration
+    public function index(){
+        return view('users.authentication');
+    }
+
+    public function login(Request $request)
     {
         print_r($request->input());
 
