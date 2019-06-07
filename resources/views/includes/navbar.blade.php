2de7367de956b02360bba35b11207d84b70202fa
ilanss <jeremie_pro@hotmail.com>
2019-06-07 13:07:11 +0200

    Fix route

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,1 +1,1 @@
-<navbar-section catalogue="{{ route('catalogue') }}" home="{{ route('home') }}" blog="{{ route('blog') }}" about="{{ route('about') }}"></navbar-section>
+<navbar-section catalogue="{{ route('products.index') }}" home="{{ route('home') }}" blog="{{ route('blog') }}" about="{{ route('about') }}"></navbar-section>
